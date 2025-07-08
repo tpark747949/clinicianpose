3c8e64d726ca2ec8eda328a2749382061851a229
Taejoon Park <tpark747949@gmail.com>
2025-07-08 17:06:52 +0900

    Fixed configs for PCT and HMP models

--- configs/pct_base_classifier.py
+++ configs/pct_base_classifier.py
@@ -32,14 +32,11 @@ log_config = dict(
     ])
 
 channel_cfg = dict(
-    num_output_channels=17,
-    dataset_joints=17,
-    dataset_channel=[
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-    ],
-    inference_channel=[
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-    ])
+    num_output_channels=133,
+    dataset_joints=133,
+    dataset_channel=[list(range(133))],
+    inference_channel=[list(range(133))]
+)
 
 data_cfg = dict(
     image_size=[256, 256],
